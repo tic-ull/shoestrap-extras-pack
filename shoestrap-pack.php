86035d5d64e46217b4f0d4d8877724244f6c30cd
aristath <aristath@gmail.com>
2014-03-28 11:17:20 +0200

    added plugin info

--- shoestrap-pack.php
+++ shoestrap-pack.php
@@ -1,1 +1,11 @@
-shoestrap-pack.php
+<?php
+
+/*
+Plugin Name: Shoestrap Extras Pack
+Plugin URI: http://wpmu.io
+Description: Shoestrap Customizations and additions
+Version: 0.3
+Author: Aristeides Stathopoulos
+Author URI:  http://aristeides.com
+*/
+
